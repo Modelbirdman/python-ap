a593e748c5d6d8962fe5cefa58ce9ecac1c924c3
Pierre Escudié <pierre8escudie@gmail.com>
2024-01-04 22:08:18 +0100

    premier code de gamelife

--- gamelife.py
+++ gamelife.py
@@ -3,20 +3,18 @@ import argparse
 import sys
 import logging 
 
-pg.init()
-clock = pg.time.Clock()
+def read_args():
 
-SCREENWIDTH=800
-SCREENHEIGHT=600
-FPS=10
-BLACK=(0,0,0)
-WHITE=(255,255,255)
-STEPS=20
+    parser = argparse.ArgumentParser(description='Arguements du jeu')
+    parser.add_argument('--width', type=int, help="Longueur du quadrillage",default=800)
+    parser.add_argument('--height', type=int, help="Hauteur du quadrillage",default=600)
+    parser.add_argument('-f', type=int, help="Nombre de frames par seconde",default=10)
+    parser.add_argument('-m', type=int, help="Nombre d'étapes de la simulation",default=20)
 
-Flag=True
+    parser.add_argument('-d', help='Affichage de pygame', action='store_true')
 
-screen = pg.display.set_mode( (SCREENWIDTH, SCREENHEIGHT) )  #Création du quadrillage
-screen.fill( WHITE )
+    args = parser.parse_args()
+    return args
 
 def nb_voisin_vivant(tab,i,j):
     res=0
@@ -27,10 +25,51 @@ def nb_voisin_vivant(tab,i,j):
                     if tab[ligne][colonne]==1:
                         res=res+1
     return res 
+
+def draw_screen(TAILLECAR,tab,WHITE,BLACK):
+    screen.fill( WHITE )
+    for i in range(len(tab)):
+        for j in range(len(tab[0])):
+            if tab[i][j]==1:
+                carre = pg.Rect(i*TAILLECAR,j*TAILLECAR,TAILLECAR, TAILLECAR)
+                pg.draw.rect(screen, BLACK, carre)
+
+def update_tab(tab):
+    tabstock=tab.copy()
+    for i in range(len(tab)):
+        for j in range(len(tab[0])):
+            nb=nb_voisin_vivant(tabstock,i,j)
+            if tabstock[i][j]==1:
+                if nb<2:
+                    tab[i][j]=0
+                elif nb>3:
+                    tab[i][j]=0
+            else:
+                if nb==3:
+                    tab[i][j]=1
+
+
+args=read_args() 
+
+
+SCREENWIDTH=args.width
+SCREENHEIGHT=args.height
+TAILLECAR=10
+FPS=args.f
+BLACK=(0,0,0)
+WHITE=(255,255,255)
+STEPS=args.m
+
+Flag=True
+step=0
+
+if args.d:
+    pg.init()
+    clock = pg.time.Clock()
+    screen = pg.display.set_mode( (SCREENWIDTH, SCREENHEIGHT) )  #Création du quadrillage
+    screen.fill( WHITE )
+
     
-
-
-
 with open('gamelife.txt','r') as f:
     tab=[]
     i=0
@@ -40,36 +79,36 @@ with open('gamelife.txt','r') as f:
         for nb in line:
             tab[i].append(int(nb))
         i=i+1
-    for loop in range(STEPS):
-        tabstock=tab.copy()
-        for i in range(len(tab)):
-            for j in range(len(tab[0])):
-                nb=nb_voisin_vivant(tabstock,i,j)
-                if tabstock[i][j]==1:
-                    if nb<2:
-                        tab[i][j]=0
-                    elif nb>3:
-                        tab[i][j]=0
-                else:
-                    if nb==3:
-                        tab[i][j]=1
-                        
+    
+if args.d:
+    while Flag==True:
+        clock.tick(FPS)
+
+        update_tab(tab)
+
+        for event in pg.event.get():
+            if event.type == pg.KEYDOWN: 
+                if event.key == pg.K_q:
+                    Flag=False
+            if event.type == pg.QUIT:
+                Flag=False
+
+        draw_screen(TAILLECAR,tab,WHITE,BLACK)
+        pg.display.update()
+
+else:
+    while step<STEPS:
+        if step<STEPS:
+            update_tab(tab)
+            step=step+1
+    
+with open('gamsortie.txt','w') as f:
+    for i in range(len(tab)):
+        for j in range(len(tab[0])):
+            f.write(str(tab[i][j]))
+        f.write('\n')
+
+quit()
 
 #tab=[[0,1,0],[0,0,0],[1,0,1]]
 #print(nb_voisin_vivant(tab,2,2))
-
-      
-
-    
-
-while Flag==True:
-    clock.tick(10)
-    for event in pg.event.get():
-        if event.type == pg.KEYDOWN: 
-            if event.key == pg.K_q:
-                Flag=False
-        if event.type == pg.QUIT:
-            Flag=False
-    pg.display.update()
-
-quit()
