4b3bda214e7199ca0c7fe938d92f849a1ce59644
Pierre Escudié <pierre8escudie@gmail.com>
2024-01-16 14:37:20 +0100

    mise a jour des tests adn

--- adn/test_adn.py
+++ adn/test_adn.py
@@ -4,10 +4,10 @@ import adn
 #PYTHONPATH=.. python -m pytest nom du fichier ou . (répertoire courant)
 #python 
 
-print(adn.remplir_tableau("ATA","AA"))
+#print(adn.remplir_tableau("AT","AG",1,1,2))
 
 def test_tableau():
-    assert adn.remplir_tableau("AT","AG",1,1,2)==[[(0, (0, 0)), (-1, (0, 0))], [(-1, (0, 0)), (1, (0, 0))]]
-    assert adn.remplir_tableau("ATA","AA",1,1,2)==[[(0, (0, 0)), (-1, (0, 0)), (-2, (0, 1))], [(-1, (0, 0)), (1, (0, 0)), (-1, (1, 1))]]
+    assert adn.remplir_tableau("AT","AG",1,1,2)==[[(0, (0, 0)), (-2, (0, 0))], [(-2, (0, 0)), (1, (0, 0))]]
+    assert adn.remplir_tableau("ATA","AA",1,1,2)==[[(0, (0, 0)), (-2, (0, 0)), (-4, (0, 1))], [(-2, (0, 0)), (1, (0, 0)), (-1, (1, 1))]]
     
 
